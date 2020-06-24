a3f33cd6fd459ceb5d8a0bc6319ba42d49f86bff
Matt Phillips <49458212+Emphillips@users.noreply.github.com>
2020-06-24 15:06:47 -0500

    Updated Palindrome
    Create Palindrome practice.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,10 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-    const arr = str.split('');
-    arr.reverse();
-    return arr.join('');
-}
+// function reverse(str) {
+//     const arr = str.split('');
+//     arr.reverse();
+//     return arr.join('');
+// }
+// --------------------------------------
+// function reverse(str) {
+//     return str
+//         .split('')
+//         .reverse()
+//         .join('');
+// }
+// -------------------------------------
 
+
+// --- No reverse helper ---  For Loop
+
+
+
+
+function reverse(str) {
+// Create an empty string called 'reversed'
+let reversed = '';
+
+// For each character in the provided string.
+// take the character and add it to the start of 'reversed'
+for (let character of str) {
+    reversed = character + reversed;
+    }
+// Return the variable 'reversed'
+    return reversed;
+}
 module.exports = reverse;
